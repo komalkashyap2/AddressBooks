8d9ad0615afc653bbd067e15961202bd267c1923
komalkashyap <komalkashyapmajri403@gmail.com>
2021-12-05 13:08:04 +0530

    [KOMAL KASHYAP] CREATE A CONTACT IN ADDRESS BOOK

---
+++ AddressBooks/AddressBooks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBooks
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBooks.AddrBook.GetCustomer();
+            AddressBooks.AddrBook.ListingPeople();
+
+        }
+    }
+}
